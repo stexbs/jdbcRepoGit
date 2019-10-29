cb45c9330b02c2582310fba2cf55ebcd003a7126
UC2 <UC2@UC5PC12>
2019-10-29 18:25:43 +0100

    final

--- src/jdbc/JdbcProject.java
+++ src/jdbc/JdbcProject.java
@@ -1,5 +1,6 @@
 package jdbc;
 
+import java.util.List;
 import java.util.Scanner;
 import controller.MetodeJdbc;
 import model.Kurs;
@@ -11,29 +12,37 @@ public class JdbcProject {
 		
 
 		MetodeJdbc metode = new MetodeJdbc();
-		
-		Scanner scanner = new Scanner (System.in);
-		System.out.println("Unesite id");
-		String id = scanner.nextLine();
-		
+		/*
+		 * Scanner scanner = new Scanner(System.in);
+		 * System.out.println("Unesite id usera:"); int id =
+		 * Integer.parseInt(scanner.nextLine()); scanner.close();
+		 */
+		/*
+		 * Kurs k = metode.vratiKursPoId(3);
+		 * 
+		 * System.out.println("Id: " + k.getIdKursa()); System.out.println("Ime: " +
+		 * k.getImeKursa()); System.out.println("Cena: " + k.getCena());
+		 */
 		
 		/*
-		Kurs k = metode.vratiKursPoId(3);
-		
-		System.out.println("Id: " + k.getIdKursa());
-		System.out.println("Ime: " + k.getImeKursa());
-		System.out.println("Cena: " + k.getCena());
-		*/
-		
-		
-		
-		User user = metode.vratiUseraPoId(Integer.parseInt(id));
-		System.out.println("ID:  " + user.getIdUser());
-		System.out.println("Username :  " + user.getUserName());
-		System.out.println("Password:  " + user.getPassword());
-		System.out.println("Maticni broj:  " + user.getMaticniBroj());
+		 * User user = metode.vratiUseraPoId(id);
+		 * 
+		 * if(user.getIdUser() != 0) { System.out.println("ID: " + user.getIdUser());
+		 * System.out.println("USER NAME: " + user.getUserName());
+		 * System.out.println("PASSWORD: " + user.getPassword());
+		 * System.out.println("MATBR: " + user.getMaticniBroj()); }else {
+		 * System.out.println("Ne postoji taj user!"); }
+		 */
 		
 		
+		List<User> lstUsers = metode.vratiSveUsere();
+		
+		for(User u: lstUsers) {
+			System.out.println("ID: " +u.getIdUser());
+			System.out.println("USER NAME: " + u.getUserName());
+			System.out.println("PASSWORD: " + u.getPassword());
+			System.out.println("MATBR: " + u.getMaticniBroj());
+		}
 		
 		
 
