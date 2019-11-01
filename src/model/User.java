5e93eb67d99b04f3ae4aa42217c13eecf435b73b
UC2 <UC2@UC5PC12>
2019-11-01 18:27:18 +0100

    final

--- src/model/User.java
+++ src/model/User.java
@@ -2,50 +2,6 @@ package model;
 
 public class User {
 	
-<<<<<<< HEAD
-	
-	private int idUser;
-	private String userName;
-	private String password;
-	private int maticniBroj;
-	
-	
-	
-	public int getIdUser() {
-		return idUser;
-	}
-	public void setIdUser(int idUser) {
-		this.idUser = idUser;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getMaticniBroj() {
-		return maticniBroj;
-	}
-	public void setMaticniBroj(int maticniBroj) {
-		this.maticniBroj = maticniBroj;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-
-}
-=======
 	private int idUser;
 	private String userName;
 	private String password;
@@ -81,4 +37,3 @@ public class User {
 	
 
 }
->>>>>>> branch 'master' of https://github.com/stexbs/jdbcRepoGit.git
